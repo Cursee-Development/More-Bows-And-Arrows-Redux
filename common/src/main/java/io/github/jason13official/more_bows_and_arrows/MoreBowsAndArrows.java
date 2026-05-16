7b76f7d56b3aac6704ca51f86cc1c3e7171953f7
jason13official <jason13official@gmail.com>
2026-05-15 20:41:21 -0400

    make arrows projectiles

--- common/src/main/java/io/github/jason13official/more_bows_and_arrows/MoreBowsAndArrows.java
+++ common/src/main/java/io/github/jason13official/more_bows_and_arrows/MoreBowsAndArrows.java
@@ -1,11 +1,14 @@
 package io.github.jason13official.more_bows_and_arrows;
 
 import io.github.jason13official.more_bows_and_arrows.impl.common.ModConfig;
+import io.github.jason13official.more_bows_and_arrows.impl.common.registry.ModItems;
 import io.github.jason13official.more_bows_and_arrows.platform.Services;
 import java.util.function.BiConsumer;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.Identifier;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.DispenserBlock;
 
 public class MoreBowsAndArrows {
 
@@ -16,6 +19,10 @@ public class MoreBowsAndArrows {
   public static void init() {
 
     ModConfig.load(Services.PLATFORM.getConfigDirectory());
+
+    ModItems.ARROWS.forEach((identifier, item) -> {
+      DispenserBlock.registerProjectileBehavior(item);
+    });
   }
 
   public static Identifier identifier(final String path) {
