b363c98b702fe6945b1214df5cd78dd1a966ecf5
jason13official <jason13official@gmail.com>
2026-05-17 18:10:59 -0400

    fixes, bump version

--- common/src/main/java/io/github/jason13official/more_bows_and_arrows/impl/common/registry/ModTabs.java
+++ common/src/main/java/io/github/jason13official/more_bows_and_arrows/impl/common/registry/ModTabs.java
@@ -2,13 +2,20 @@ package io.github.jason13official.more_bows_and_arrows.impl.common.registry;
 
 import io.github.jason13official.more_bows_and_arrows.Constants;
 import io.github.jason13official.more_bows_and_arrows.MoreBowsAndArrows;
+import io.github.jason13official.more_bows_and_arrows.impl.common.item.bow.BowType;
 import io.github.jason13official.more_bows_and_arrows.platform.Services;
 import java.util.function.BiConsumer;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTab.TabVisibility;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.item.enchantment.EnchantmentInstance;
 
 public class ModTabs {
 
@@ -19,12 +26,36 @@ public class ModTabs {
     MORE_BOWS_AND_ARROWS = Services.PLATFORM.tabBuilder()
         .icon(() -> new ItemStack(Items.BOW))
         .title(Component.translatable("itemGroup.moreBowsAndArrows"))
-        .displayItems((itemDisplayParameters, output) -> {
-          ModItems.BOWS.forEach((identifier, item) -> output.accept(item));
+        .displayItems((parameters, output) -> {
+          ModItems.BOWS.forEach((identifier, item) -> {
+            if (identifier != BowType.NORMAL) {
+              output.accept(item);
+            }
+          });
+          parameters.holders().lookup(Registries.ENCHANTMENT).ifPresent((enchantments) -> {
+            generateEnchantmentBookTypesOnlyMaxLevel(output, enchantments, TabVisibility.PARENT_TAB_ONLY);
+          });
           ModItems.ARROWS.forEach((identifier, item) -> output.accept(item));
         })
         .build();
 
     consumer.accept(MORE_BOWS_AND_ARROWS, MoreBowsAndArrows.identifier(Constants.MOD_ID));
+  }
+
+  private static void generateEnchantmentBookTypesOnlyMaxLevel(CreativeModeTab.Output output, HolderLookup<Enchantment> enchantments, CreativeModeTab.TabVisibility tabVisibility) {
+    enchantments.listElements()
+        .map((enchantment) -> {
+
+          if (!enchantment.key().identifier().getNamespace().equalsIgnoreCase(Constants.MOD_ID)) {
+            return ItemStack.EMPTY;
+          }
+
+          return EnchantmentHelper.createBook(new EnchantmentInstance(enchantment, enchantment.value().getMaxLevel()));
+        })
+        .forEach((stack) -> {
+          if (!stack.isEmpty()) {
+            output.accept(stack, tabVisibility);
+          }
+        });
   }
 }
