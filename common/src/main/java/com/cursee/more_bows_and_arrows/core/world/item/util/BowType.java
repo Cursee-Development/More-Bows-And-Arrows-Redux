2dadb2f2692832834d3d6a10ecb2e6727d2cfab0
jason13official <jason13official@gmail.com>
2026-05-22 00:17:52 -0400

    5.0.4 remove test and test2 bowtype

--- common/src/main/java/com/cursee/more_bows_and_arrows/core/world/item/util/BowType.java
+++ common/src/main/java/com/cursee/more_bows_and_arrows/core/world/item/util/BowType.java
@@ -20,8 +20,8 @@ import java.util.function.Supplier;
 @SuppressWarnings("deprecation")
 public enum BowType implements Tier {
 
-    TEST(1, 0.0f, 0.0f, 0, 0, () -> Ingredient.of(Blocks.OAK_PLANKS)),
-    TEST2(1, 0.0f, 0.0f, 0, 0, () -> Ingredient.of(Blocks.OAK_PLANKS)),
+    // TEST(1, 0.0f, 0.0f, 0, 0, () -> Ingredient.of(Blocks.OAK_PLANKS)),
+    // TEST2(1, 0.0f, 0.0f, 0, 0, () -> Ingredient.of(Blocks.OAK_PLANKS)),
 
     NORMAL(1, 0.0f, 0.0f, 0, 0, () -> Ingredient.of(Blocks.OAK_PLANKS)),
     NOCTURNAL(369, 2.0f, 4.0f, 3, 20, () -> Ingredient.of(Items.PHANTOM_MEMBRANE)),
